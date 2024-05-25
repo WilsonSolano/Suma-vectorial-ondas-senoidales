fad1f943f37bcc7bbe5ab1df970a4366da8c5ea5
Orisama03 <112028499+Orisama03@users.noreply.github.com>
2024-05-25 00:31:12 -0600

    por si acaso

--- ondas.Designer.cs
+++ ondas.Designer.cs
@@ -28,31 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            quees = new NAudio.Gui.WaveformPainter();
+            pictureBox = new PictureBox();
+            sliderVolumen = new MaterialSkin.Controls.MaterialSlider();
+            sliderFrecuencia = new MaterialSkin.Controls.MaterialSlider();
+            btnIniciar = new MaterialSkin.Controls.MaterialButton();
+            btnParar = new MaterialSkin.Controls.MaterialButton();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
             SuspendLayout();
             // 
-            // quees
+            // pictureBox
             // 
-            quees.BackColor = SystemColors.ActiveCaptionText;
-            quees.Location = new Point(823, 163);
-            quees.Name = "quees";
-            quees.Size = new Size(316, 177);
-            quees.TabIndex = 0;
-            quees.Text = "waveformPainter1";
+            pictureBox.Location = new Point(55, 328);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(1195, 321);
+            pictureBox.TabIndex = 0;
+            pictureBox.TabStop = false;
+            // 
+            // sliderVolumen
+            // 
+            sliderVolumen.Depth = 0;
+            sliderVolumen.Font = new Font("Roboto Medium", 20F, FontStyle.Bold, GraphicsUnit.Pixel);
+            sliderVolumen.FontType = MaterialSkin.MaterialSkinManager.fontType.H6;
+            sliderVolumen.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            sliderVolumen.Location = new Point(115, 199);
+            sliderVolumen.MouseState = MaterialSkin.MouseState.HOVER;
+            sliderVolumen.Name = "sliderVolumen";
+            sliderVolumen.Size = new Size(507, 40);
+            sliderVolumen.TabIndex = 1;
+            sliderVolumen.Text = "Volumen";
+            sliderVolumen.onValueChanged += sliderVolumen_onValueChanged;
+            // 
+            // sliderFrecuencia
+            // 
+            sliderFrecuencia.Depth = 0;
+            sliderFrecuencia.Font = new Font("Roboto Medium", 20F, FontStyle.Bold, GraphicsUnit.Pixel);
+            sliderFrecuencia.FontType = MaterialSkin.MaterialSkinManager.fontType.H6;
+            sliderFrecuencia.ForeColor = Color.FromArgb(222, 0, 0, 0);
+            sliderFrecuencia.Location = new Point(94, 153);
+            sliderFrecuencia.MouseState = MaterialSkin.MouseState.HOVER;
+            sliderFrecuencia.Name = "sliderFrecuencia";
+            sliderFrecuencia.RangeMax = 4000;
+            sliderFrecuencia.Size = new Size(538, 40);
+            sliderFrecuencia.TabIndex = 2;
+            sliderFrecuencia.Text = "Frecuencia";
+            sliderFrecuencia.Value = 1000;
+            sliderFrecuencia.onValueChanged += sliderFrecuencia_onValueChanged;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnIniciar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            btnIniciar.Depth = 0;
+            btnIniciar.HighEmphasis = true;
+            btnIniciar.Icon = null;
+            btnIniciar.Location = new Point(170, 260);
+            btnIniciar.Margin = new Padding(4, 6, 4, 6);
+            btnIniciar.MouseState = MaterialSkin.MouseState.HOVER;
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.NoAccentTextColor = Color.Empty;
+            btnIniciar.Size = new Size(158, 36);
+            btnIniciar.TabIndex = 3;
+            btnIniciar.Text = "materialButton1";
+            btnIniciar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            btnIniciar.UseAccentColor = false;
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click_1;
+            // 
+            // btnParar
+            // 
+            btnParar.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnParar.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            btnParar.Depth = 0;
+            btnParar.HighEmphasis = true;
+            btnParar.Icon = null;
+            btnParar.Location = new Point(379, 269);
+            btnParar.Margin = new Padding(4, 6, 4, 6);
+            btnParar.MouseState = MaterialSkin.MouseState.HOVER;
+            btnParar.Name = "btnParar";
+            btnParar.NoAccentTextColor = Color.Empty;
+            btnParar.Size = new Size(158, 36);
+            btnParar.TabIndex = 4;
+            btnParar.Text = "materialButton2";
+            btnParar.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            btnParar.UseAccentColor = false;
+            btnParar.UseVisualStyleBackColor = true;
             // 
             // ondas
             // 
             AutoScaleDimensions = new SizeF(8F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1329, 680);
-            Controls.Add(quees);
+            Controls.Add(btnParar);
+            Controls.Add(btnIniciar);
+            Controls.Add(sliderFrecuencia);
+            Controls.Add(sliderVolumen);
+            Controls.Add(pictureBox);
             Name = "ondas";
             Text = "ondas";
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private NAudio.Gui.WaveformPainter quees;
+        private PictureBox pictureBox;
+        private MaterialSkin.Controls.MaterialSlider sliderVolumen;
+        private MaterialSkin.Controls.MaterialSlider sliderFrecuencia;
+        private MaterialSkin.Controls.MaterialButton btnIniciar;
+        private MaterialSkin.Controls.MaterialButton btnParar;
     }
 }
--- ondas.cs
+++ ondas.cs
@@ -9,11 +9,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using Timer = System.Windows.Forms.Timer;
 
 namespace Calculadoradevectore
 {
     public partial class ondas : MaterialSkin.Controls.MaterialForm
     {
+        private WaveOutEvent waveOut = new WaveOutEvent();
+        private ondaSenoidalClass ondaSenoidal = new ondaSenoidalClass(44100);
+        private bool noSuena = true;
+        private float time;
+        private const float timeIncrement = 0.02f;
+        private Timer timer;
+
         public ondas()
         {
             var materialSkinManager = MaterialSkinManager.Instance;
@@ -21,27 +30,71 @@ namespace Calculadoradevectore
             materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
             materialSkinManager.ColorScheme = new ColorScheme(Primary.Cyan400, Primary.Cyan200, Primary.Cyan50, Accent.Lime400, TextShade.BLACK);
             InitializeComponent();
+        }
 
-            //SineWaveProvider32 provider = new SineWaveProvider32();
-            //WaveOut waveOut = new WaveOut();
-            //waveOut.Init(provider);
-            //waveOut.Play();
+        private void generarSonido(float volum, int frecuen, WaveOutEvent waveOutF, ondaSenoidalClass onda)
+        {
+            volum = volum / 100;
+
+            onda.Frecuencia = frecuen;
+            onda.Volumen = volum;
+            // Inicializar el dispositivo de salida de audio
+            waveOutF.Init(onda);
+        }
+
+        private void DrawSineWave(PictureBox pictureBox)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            g.Clear(Color.White);
+
+            Pen pen = new Pen(Color.Black);
+            int width = pictureBox.Width;
+            int height = pictureBox.Height;
+            int midHeight = height / 2;
+
+            float amplitude = ondaSenoidal.Volumen /** height / 2*/; // Escalar amplitud para ajustarse al gráfico
+            float frequency = (float)ondaSenoidal.Frecuencia;
+            float sampleRate = (float)ondaSenoidal.WaveFormat.SampleRate;
+
+            PointF[] points = new PointF[width];
+            for (int x = 0; x < width; x++)
+            {
+                float t = (time + x / sampleRate);
+                float y = amplitude * (float)Math.Sin(2 * Math.PI * frequency * t);
+                points[x] = new PointF(x, midHeight - y);
+            }
+
+            g.DrawLines(pen, points);
+        }
+
+        private void sliderFrecuencia_onValueChanged(object sender, int newValue)
+        {
+            ondaSenoidal.Frecuencia = Convert.ToInt32(sliderFrecuencia.Value);
+            DrawSineWave(pictureBox);
+        }
+
+        private void sliderVolumen_onValueChanged(object sender, int newValue)
+        {
+            ondaSenoidal.Volumen = Convert.ToInt32(sliderVolumen.Value);
+            DrawSineWave(pictureBox);
+        }
+
+        private void btnIniciar_Click_1(object sender, EventArgs e)
+        {
+            if (noSuena)
+            {
+                generarSonido(Convert.ToInt32(sliderVolumen.Value), Convert.ToInt32(sliderFrecuencia.Value), waveOut, ondaSenoidal);
+                waveOut.Play();
+            }
+            noSuena = false;
+
+            DrawSineWave(pictureBox);
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            waveOut.Stop();
+            noSuena = true;
         }
     }
-
-    //public class SineWaveProvider32 : WaveProvider32
-    //{
-    //    private float amplitude = 0.5f;
-    //    private float frequency = 440f; // 440 Hz is the frequency of the note A above middle C
-
-    //    public override int Read(float[] buffer, int offset, int sampleCount)
-    //    {
-    //        int sampleRate = WaveFormat.SampleRate;
-    //        for (int n = 0; n < sampleCount; n++)
-    //        {
-    //            buffer[n + offset] = (float)(amplitude * Math.Sin(2 * Math.PI * frequency * n / sampleRate));
-    //        }
-    //        return sampleCount;
-    //    }
-    //}
 }
