91a6f93e20d9f23c20e4310c6ee177b5bb057404
Orisama03 <112028499+Orisama03@users.noreply.github.com>
2024-05-24 20:11:27 -0600

    agregamos liberias para NAudio y la primer class junto a un formulario

---
+++ ondas.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Calculadoradevectore
+{
+    partial class ondas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            quees = new NAudio.Gui.WaveformPainter();
+            SuspendLayout();
+            // 
+            // quees
+            // 
+            quees.BackColor = SystemColors.ActiveCaptionText;
+            quees.Location = new Point(823, 163);
+            quees.Name = "quees";
+            quees.Size = new Size(316, 177);
+            quees.TabIndex = 0;
+            quees.Text = "waveformPainter1";
+            // 
+            // ondas
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1329, 680);
+            Controls.Add(quees);
+            Name = "ondas";
+            Text = "ondas";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private NAudio.Gui.WaveformPainter quees;
+    }
+}
---
+++ ondas.cs
@@ -0,0 +1,47 @@
+﻿using MaterialSkin;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadoradevectore
+{
+    public partial class ondas : MaterialSkin.Controls.MaterialForm
+    {
+        public ondas()
+        {
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Cyan400, Primary.Cyan200, Primary.Cyan50, Accent.Lime400, TextShade.BLACK);
+            InitializeComponent();
+
+            //SineWaveProvider32 provider = new SineWaveProvider32();
+            //WaveOut waveOut = new WaveOut();
+            //waveOut.Init(provider);
+            //waveOut.Play();
+        }
+    }
+
+    //public class SineWaveProvider32 : WaveProvider32
+    //{
+    //    private float amplitude = 0.5f;
+    //    private float frequency = 440f; // 440 Hz is the frequency of the note A above middle C
+
+    //    public override int Read(float[] buffer, int offset, int sampleCount)
+    //    {
+    //        int sampleRate = WaveFormat.SampleRate;
+    //        for (int n = 0; n < sampleCount; n++)
+    //        {
+    //            buffer[n + offset] = (float)(amplitude * Math.Sin(2 * Math.PI * frequency * n / sampleRate));
+    //        }
+    //        return sampleCount;
+    //    }
+    //}
+}
